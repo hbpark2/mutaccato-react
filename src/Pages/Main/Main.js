397b7c3151bc339a43bd0b098501055d7763edd0
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-01-30 18:57:21 +0900

    manifest

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+
+const Container = styled.article``
+
 const Main = () => {
-  return <div id="content"></div>
+  return (
+    <Container id="content">
+      <h2 className="hidden">CONTENT SECTION</h2>
+    </Container>
+  )
 }
 
 export default Main
