9f4395c20abe1d915ffd07565806bea7424e60f8
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-12-03 15:31:00 +0900

    package, personal image layout\

--- src/Pages/PackageDesign/index.js
+++ src/Pages/PackageDesign/index.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import {LightgalleryProvider, LightgalleryItem, useLightgallery} from 'react-lightgallery'
-import {packageData} from '../../ImageData/packageData'
-import LayerBox from '../../Components/Common/LayerBox'
-import {Container, Image, ListWrap, OpenGalleryBtn} from '../../Components/Common/LightgalleryGrid'
+import React from "react";
+import {
+  LightgalleryProvider,
+  LightgalleryItem,
+  useLightgallery,
+} from "react-lightgallery";
+import { packageData } from "../../ImageData/packageData";
+import LayerBox from "../../Components/Common/LayerBox";
+import {
+  Container,
+  ListWrap,
+  OpenGalleryBtn,
+} from "../../Components/Common/LightgalleryGrid";
+import styled from "styled-components";
 
+const Image = styled.div`
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
 const Package = () => {
-  const PhotoItem = ({image, thumb, group}) => (
+  const PhotoItem = ({ image, thumb, group }) => (
     <OpenGalleryBtn>
       <LightgalleryItem group={group} src={image} thumb={thumb}>
-        <Image bg={image} />
+        <Image>
+          <img src={image} alt="개인작품이미지" />
+        </Image>{" "}
         <LayerBox />
       </LightgalleryItem>
     </OpenGalleryBtn>
-  )
+  );
 
   return (
     <Container>
       <ListWrap>
         <LightgalleryProvider>
           {packageData.map((p, idx) => (
-            <PhotoItem key={idx} image={p} group={'01'} />
+            <PhotoItem key={idx} image={p} group={"01"} />
           ))}
         </LightgalleryProvider>
       </ListWrap>
     </Container>
-  )
-}
-export default Package
+  );
+};
+export default Package;
--- src/Pages/PersonalWorks/index.js
+++ src/Pages/PersonalWorks/index.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import {LightgalleryProvider, LightgalleryItem, useLightgallery} from 'react-lightgallery'
-import LayerBox from '../../Components/Common/LayerBox'
-import {Container, Image, ListWrap, OpenGalleryBtn} from '../../Components/Common/LightgalleryGrid'
-import {personalWorksData} from '../../ImageData/personalWorksData'
+import React from "react";
+import {
+  LightgalleryProvider,
+  LightgalleryItem,
+  useLightgallery,
+} from "react-lightgallery";
+import LayerBox from "../../Components/Common/LayerBox";
+import {
+  Container,
+  ListWrap,
+  OpenGalleryBtn,
+} from "../../Components/Common/LightgalleryGrid";
+import { personalWorksData } from "../../ImageData/personalWorksData";
+import styled from "styled-components";
+const Image = styled.div`
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const PersonalWorks = () => {
-  const PhotoItem = ({image, thumb, group}) => (
+  const PhotoItem = ({ image, thumb, group }) => (
     <OpenGalleryBtn>
       <LightgalleryItem group={group} src={image} thumb={thumb}>
-        <Image bg={image} />
+        <Image>
+          <img src={image} alt="개인작품이미지" />
+        </Image>
         <LayerBox />
       </LightgalleryItem>
     </OpenGalleryBtn>
-  )
+  );
 
   return (
     <Container>
       <ListWrap>
         <LightgalleryProvider>
           {personalWorksData.map((p, idx) => (
-            <PhotoItem key={idx} image={p} group={'01'} />
+            <PhotoItem key={idx} image={p} group={"01"} />
           ))}
         </LightgalleryProvider>
       </ListWrap>
     </Container>
-  )
-}
-export default PersonalWorks
+  );
+};
+export default PersonalWorks;
